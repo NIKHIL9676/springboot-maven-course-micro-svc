e9a37dff94076fe5b5c74ec1229f508dd9c123f8
NIKHIL9676 <75869382+NIKHIL9676@users.noreply.github.com>
2021-12-31 15:26:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,9 @@
-terraform {
+ terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
+    azurerm = "2.52.0"
   }
 }
+
 
 provider "azurerm" {
   features {}
